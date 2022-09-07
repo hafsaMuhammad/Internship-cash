40da5de49317a87aa11be7c9f60f08adc5261031
HafsaMuhammad <hafsa.mohamed1999@gmail.com>
2022-09-07 20:37:47 +0200

    edited files

--- VodafoneCash/settings.py
+++ VodafoneCash/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'VodafoneCash.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('SQL_DB'),
+        'NAME': os.environ.get('SQL_DB', 'db'),
         'USER': os.environ.get('SQL_USER'),
         'PASSWORD': os.environ.get('SQL_PASSWORD'),
         'HOST': os.environ.get('SQL_HOST'),
